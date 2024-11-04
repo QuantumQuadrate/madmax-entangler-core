4b3ef19de49644f4bc254c5374f9acc3c42eeded
Brad Bondurant <brad.bondurant@duke.edu>
2024-11-04 11:13:14 -0500

    bump dynaconf version to support python 3.11

--- nix/entangler-dependencies.nix
+++ nix/entangler-dependencies.nix
@@ -3,10 +3,10 @@
 {
   dynaconf = python3Packages.buildPythonPackage rec {
     pname = "dynaconf";
-    version = "3.1.7";
+    version = "3.2.6";
     src = python3Packages.fetchPypi {
       inherit pname version;
-      sha256 = "e9d80b46ba4d9372f2f40c812594c963f74178140c0b596e57f2881001fc4d35";
+      sha256 = "74cc1897396380bb957730eb341cc0976ee9c38bbcb53d3307c50caed0aedfb8";
     };
 
     propagatedBuildInputs = with python3Packages; [
