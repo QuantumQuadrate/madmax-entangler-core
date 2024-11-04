306cb360da6aacf40180d07c3c6e073ec6c43ffe
Brad Bondurant <brad.bondurant@duke.edu>
2024-11-04 11:27:20 -0500

    bump entangler version to 1.4.0

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "entangler";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = lib.cleanSource ./..;
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="entangler",
-        version="1.3.0",
+        version="1.4.0",
         packages=setuptools.find_packages(),
         requirements=["artiq>=5", "dynaconf>=3", "mergedeep", "numpy"],
         url="https://github.com/drewrisinger/entangler-core",
