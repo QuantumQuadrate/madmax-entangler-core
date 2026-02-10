5960e0be2cc1cf4bcce815512df618924a269c28
Moh Kashani <mkashani.phd@gmail.com>
2026-02-09 17:25:21 -0800

    modify the config.py to correctly load the setting.toml

--- entangler/config.py
+++ entangler/config.py
@@ -3,6 +3,13 @@ import pathlib
 from dynaconf import Dynaconf
 
 
+# settings = Dynaconf(
+#     settings_files=[pathlib.Path(__file__).parent / "settings.toml"],
+# )
+
+
 settings = Dynaconf(
-    settings_files=[pathlib.Path(__file__).parent / "settings.toml"],
+    settings_files=["settings.toml", ".secrets.toml"],
+    load_dotenv=True,
+    environments=True,
 )
