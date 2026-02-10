4e317a7265ee92c0cb737dbd2604c744e2a14108
Moh Kashani <mkashani.phd@gmail.com>
2026-02-09 17:30:06 -0800

    revert the change I will try to make that UV also brings the setting.toml to thevirtual env

--- entangler/config.py
+++ entangler/config.py
@@ -3,13 +3,7 @@ import pathlib
 from dynaconf import Dynaconf
 
 
-# settings = Dynaconf(
-#     settings_files=[pathlib.Path(__file__).parent / "settings.toml"],
-# )
-
-
 settings = Dynaconf(
-    settings_files=["settings.toml", ".secrets.toml"],
-    load_dotenv=True,
-    environments=True,
+    settings_files=[pathlib.Path(__file__).parent / "settings.toml"],
 )
+
